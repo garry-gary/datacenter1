bad6692b0232996f3bfb1b530b3d2d9e273927b6
cuijiawe <2679902103@qq.com>
2023-03-02 14:45:02 +0800

    test

---
+++ idc1/c/crtsurfdata1.cpp
@@ -0,0 +1,32 @@
+#include "_public.h"
+CLogFile logfile(10);
+int main(int argc, char *argv[])
+{
+    //inifile outpath logfile
+    if(argc!=4)
+    {
+        printf("Using:./crtsurfdata1 inifile outpath logfile\n");
+       printf("Example:/project/idc1/bin/crtsurfdata1 /project/idc1/ini/stcode.ini /tmp/surfdata /log/idc/crtsurfdata1.log\n\n");
+
+        printf("inifile 全国气象站点参数文件名\n");
+        printf("outpath 全国气象站点数据文件存放的目录\n");
+        printf("logfile 本程序运行的日志文件名\n\n");
+
+        return -1;
+    }	
+
+    if(logfile.Open(argv[3]) == false){
+        printf("logfile.Open(%s) failed.\n",argv[3]);
+        return -1;
+    }
+
+    logfile.Write("crtsurfdata1 开始运行\n");
+
+    //这里插入业务代码
+    for(int ii=0; ii < 10000000; ii++)
+        logfile.Write("这是第%010d条日志记录\n", ii);
+
+    logfile.Write("crtsurfdata1 结束运行\n");
+    return 0;
+}
+
